acd33cc2626a4ae9abfca3fbf093561dd3773f5d
naziha <2111467@iub.edu.bd>
2024-12-15 01:44:51 +0600

    second commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,18 @@
-module com.example.simulatingflighttrainigacademy {
+module com.example.javafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
 
-    opens com.example.simulatingflighttrainigacademy to javafx.fxml;
-    exports com.example.simulatingflighttrainigacademy;
+    opens com.example.javafx to javafx.fxml;
+    exports com.example.javafx;
+    exports com.example.javafx.controllers;
+    opens com.example.javafx.controllers to javafx.fxml;
 }
